66689ac2d1d8973d29ebccbf9bfc836d2cd566e6
jesus212san-star <neko212jiromiya@gmail.com>
2026-04-23 08:29:34 -0600

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// Archivo app.js
+console.log("Página de la actividad 'Adivina el Número' cargada correctamente.");
+
+// Ejemplo: Al cargar la página muestra un mensaje en consola
+window.onload = function() {
+    console.log("¡Listo para revisar la tarea!");
+};
